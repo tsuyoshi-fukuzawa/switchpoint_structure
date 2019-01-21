3b451674785fba0e8b7dacf821a74c9726dc69dd
tsuyoshi_fukuzawa <tsuyoshi.fukuzawa@gmail.com>
2019-01-21 14:16:00 +0900

    upd

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,2 @@
-require 'dotenv'
-Dotenv.load
-
 lock "~> 3.11.0"
 
-set :application, "switchpoint_structure"
-set :repo_url, "git@github.com:tsuyoshi-fukuzawa/switchpoint_structure.git"
-set :branch, 'master'
-set :deploy_to, '/home/ec2-user/www/switchpoint_deploy'
-
-# rbenv
-set :default_env, { path: "/home/ec2-user/.rbenv/shims:/home/ec2-user/.rbenv/bin:$PATH" }
-set :rbenv_custom_path, '/home/ec2-user/.rbenv/bin/rbenv'
-set :rbenv_type, :user
-set :rbenv_ruby, '2.5.1'
-set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
-set :rbenv_map_bins, %w{rake gem bundle ruby rails}
-set :rbenv_roles, :all
-
-set :keep_releases, 5
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-set :linked_files, %w{.env}
